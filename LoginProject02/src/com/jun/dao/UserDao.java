d2f5173f0e11dbf14c49437579e7c7963db56b86
Yaoqing Jiang <150933538+junlint@users.noreply.github.com>
2026-09-04 20:12:55 +0800

    Delete LoginProject02 directory

--- LoginProject02/src/com/jun/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.jun.dao;
-
-import com.jun.pojo.User;
-
-public interface UserDao {
-    public abstract User selectOne(String uname, String pwd);
-}
--- LoginProject02/src/com/jun/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.jun.service;
-
-import com.jun.pojo.User;
-
-public interface UserService {
-
-    public abstract User selectOne(String uname,String pwd);
-}
